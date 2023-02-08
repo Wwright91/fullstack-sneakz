5983a53631da37e16be26f66ceb2dfa31773ede9
Wwright91 <wisdomwright-martin@pursuit.org>
2023-02-07 22:56:12 -0500

    create, read complete

--- controllers/sneakzController.js
+++ controllers/sneakzController.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const sneakz = express.Router();
 
-const { getAllSneakers } = require("../queries/sneakz");
+const {
+  getAllSneakers,
+  getOneSneaker,
+  createSneaker,
+} = require("../queries/sneakz");
 
 sneakz.get("/", async (req, res) => {
   const allSneakers = await getAllSneakers();
@@ -9,7 +13,26 @@ sneakz.get("/", async (req, res) => {
   if (allSneakers[0]) {
     res.status(200).json(allSneakers);
   } else {
-    res.status(500).json({ error: "server error" });
+    res.status(500).json({ error: "Server Error!" });
+  }
+});
+
+sneakz.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const sneaker = await getOneSneaker(id);
+  if (sneaker) {
+    res.json(sneaker);
+  } else {
+    res.status(404).json({ error: "No Sneaker Found!" });
+  }
+});
+
+sneakz.post("/", async (req, res) => {
+  try {
+    const sneaker = await createSneaker(req.body);
+    res.json(sneaker);
+  } catch (error) {
+    res.status(400).json({ error });
   }
 });
 
--- queries/sneakz.js
+++ queries/sneakz.js
@@ -9,4 +9,30 @@ const getAllSneakers = async () => {
   }
 };
 
-module.exports = { getAllSneakers };
+const getOneSneaker = async (id) => {
+  try {
+    const oneSneaker = await db.oneOrNone(
+      "SELECT * FROM sneakers WHERE id=$1",
+      id
+    );
+    return oneSneaker;
+  } catch (error) {
+    return error;
+  }
+};
+
+const createSneaker = async (sneaker) => {
+  const { price, seller_id, name, brand, color, used, img, review } = sneaker;
+
+  try {
+    const newSneaker = await db.oneOrNone(
+      "INSERT INTO sneakers (price, seller_id, name, brand, color, used, img, review) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *",
+      [price, seller_id, name, brand, color, used, img, review]
+    );
+    return newSneaker;
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = { getAllSneakers, getOneSneaker, createSneaker };
